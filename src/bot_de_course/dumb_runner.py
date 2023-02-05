ddf76e691215b80fe2c14abd0eaa8ff898d805d9
Mistayan <helixmastaz@gmail.com>
2023-02-05 19:02:10 +0100

    integration refacto _handle, _target and _path are now protected instead of private __next action is now more versatile in terms of inputs

--- src/bot_de_course/dumb_runner.py
+++ src/bot_de_course/dumb_runner.py
@@ -20,46 +20,55 @@ class RunnerAgent(pytactx.Agent):
                          server="mqtt.jusdeliens.com",
                          prompt=False,
                          verbose=False)
-        self.__path: list[tuple[str, int, int]] = []
-        self.__last: tuple[str, int, int] = None
-        self.__visited: list[str] = []
-        self.__target, self.goto_x, self.goto_y = None, None, None
+        self._target: str = None
+        self._path: list[tuple[str, int, int]] = []
 
+        self.__visited: list[str] = []
+        self.__last: tuple[str, int, int] = None
+
+    # ================================= PRIVATE METHODS ================================= #
     @property
-    def __next_action(self) -> tuple[str, int, int]:
+    def __next_action(self) -> str:
         """ Methode à mémoire.
         Renvoi la prochaine action à effectuer dans la liste self.path"""
         print("Next Action")
         if not hasattr(self, '_path_iter'):
-            self._path_iter = iter(self.__path)
+            self._path_iter = iter(self._path)
         try:
             _next = next(self._path_iter)
-            if _next and _next[0] not in self.__visited:
+            _next = str(_next if isinstance(_next, int) else _next[0] if isinstance(_next, tuple) else None)
+            if _next not in self.__visited:
+                print("Next Target : ", _next)
                 return _next
         except StopIteration:
             print("restart actions")
-            self._path_iter = iter(self.__path)
+            self._path_iter = iter(self._path)
             return self.__next_action
 
-    def __handle(self):
-        # print(self.__path)
-        if not self.goto_x:
-            self.__last = self.__target
-        if not self.goto_x or self.derniereDestinationAtteinte == self.__target:
-            print("ARRIVED @ ", self.__target)
-            self.__target, self.goto_x, self.goto_y = self.__next_action
-            self.__visited.append(self.__target)
-            print(f"deplacerVers {self.__target}")
-        self.deplacerVers(self.__target)
+    # ================================= PROTECTED METHODS ================================= #
+    def _handle(self):
+        """ Move to target.
+        If arrived at requested location, find next target to go to.
+        Save current target in visited, in case path changes [fail-safe]"""
+        if not self._target or self.derniereDestinationAtteinte == self._target:
+            print("ARRIVED @ ", self._target)
+            self.__visited.append(self._target)
+            self._target = self.__next_action
+            print(f"deplacerVers {self._target}")
+        self.deplacerVers(self._target)
 
+    # ================================= PUBLIC METHODS ================================= #
     def go(self):
-        self.__path = mapping.cities_from_game_dict(agent.jeu)
+        self._path = mapping.cities_from_game_dict(agent.jeu)
 
         while self.vie > 0:
             self.actualiser()
-            self.__handle()
+            self._handle()
+
+
+# ================================= TEST FILE ================================= #
 
 
 if __name__ == '__main__':
-    agent = RunnerAgent("Dummy-Runner0")
+    agent = RunnerAgent("Dummy-Runner")
     agent.go()
