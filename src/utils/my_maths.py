f8cf27540b28bed0f6612e5661efdb8fbf2aa4e6
Mistayan <helixmastaz@gmail.com>
2023-02-20 14:56:47 +0100

    Ajout calculate_pair_distances : calcul la distance entre deux entités et la retourne

--- src/utils/my_maths.py
+++ src/utils/my_maths.py
@@ -1,4 +1,7 @@
 import math
+from typing import Tuple, Type
+
+import numpy as np
 
 
 def euclidean_distance(a, b):
@@ -8,6 +11,17 @@ def euclidean_distance(a, b):
     :param b: to NodeB
     :return: euclidian distance between entities
     """
-    x1, y1 = (a.x, a.y)
-    x2, y2 = (b.x, b.y)
+    x1, y1 = a
+    x2, y2 = b
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def calculate_pair_distance(city1: Tuple[str, int, int], city2: Tuple[str, int, int], distance_type: Type = int):
+    """
+    calcul la distance entre deux entités et la retourne
+    :param city1: from
+    :param city2: to
+    :param distance_type: int | float | [NUMERIC VALUE]
+    :return: (from, to), distance
+    """
+    return (city1[0], city2[0]), distance_type(np.linalg.norm(np.array(city1[1:]) - np.array(city2[1:])))
