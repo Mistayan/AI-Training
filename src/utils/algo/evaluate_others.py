4d8a0a897b1ac15737f2e26c929980dd4b12fbf7
Mistayan <helixmastaz@gmail.com>
2023-02-20 14:54:15 +0100

    Utilitaire d'évaluation des autres robots de l'arène.

--- src/utils/algo/evaluate_others.py
+++ src/utils/algo/evaluate_others.py
@@ -1,25 +1,32 @@
-def smart_eval(our_bot, ref_bot):
+import logging
+
+
+def eval_fear_factor(our_bot: dict, ref_bot: dict):
     """ Evaluate the fear_factor for each bot"""
+    # extract values
+    df, mmd, ld, ad = our_bot['DIST_PENALTY'], our_bot['MAP_MAX_DISTANCE'], \
+        our_bot['LIFE_PENALTY'], our_bot['AMMO_PENALTY']
+    dist, ammo, life = ref_bot['distance'], ref_bot['ammo'], ref_bot['life']
     # find out the max factor:
-    max_factor = our_bot.DIST_PENALTY + our_bot.LIFE_PENALTY + our_bot.AMMO_PENALTY
     # penalize if bot is far away:
     # We may encounter random events that could harm us on the way
     # [ DIST >>]
-    print("#" * 12, "Eval distance")
-
-    dist_factor = our_bot.DIST_PENALTY / (ref_bot.distance /
-                                          our_bot.MAP_MAX_DISTANCE + 1)
+    dist_factor = df * (dist / mmd)
 
     # penalize if bot has high life values. Strong target means HIGH risks
     # [LIFE >>]
-    life_factor = our_bot.LIFE_PENALTY / (ref_bot.vie / 100)
+    life_factor = ld * (life / 100)
 
     # penalize if bot has low ammo
     # [<< AMMO]
-    ammo_factor = our_bot.AMMO_PENALTY * (ref_bot.ammo / 100)
-    print(f"Eval complete for {ref_bot.name} => \n",
-          f"df({ref_bot.distance}) = {dist_factor:.2f}\n",
-          f"lf({ref_bot.vie}) = {life_factor:.2f}\n",
-          f"af({ref_bot.ammo}) = {ammo_factor:2f}")
-    # ref_bot.__set_score(fear_factor)
-    return (dist_factor + life_factor + ammo_factor) / (max_factor * 3)
+    ammo_factor = ad * (ammo / 100)
+    max_factor = ((df / mmd) * (ld + ad))
+    fear_factor = (dist_factor * ((life_factor + ammo_factor) * 2))
+    logging.log(logging.DEBUG, f"""Eval complete =>
+          max  fact : {max_factor:.5f}
+          dist fact : ({dist}) = {dist_factor:.2f}
+          life fact : ({life}) = {life_factor:.2f}
+          ammo fact : ({ammo}) = {ammo_factor:.2f}
+          fear_factor : {fear_factor:.3f}
+          potential accomodation : { fear_factor * max_factor}""")
+    return min(fear_factor, 1)
