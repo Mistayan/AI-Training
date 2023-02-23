48ba01b4ca0389f34b10ab3b3475171b2ae3c6d4
Mistayan <helixmastaz@gmail.com>
2023-02-23 18:06:52 +0100

    Generalized factors for project (since it is IA oriented)

--- config.py
+++ config.py
@@ -5,4 +5,13 @@ i = 0
 for i, b in enumerate(base_dir):
     if b == "AI-Training":
         break
-base_dir = os.path.join('\\'.join(base_dir[0: i + 1]))
+base_dir = os.path.join("\\".join(base_dir[0: i + 1]))
+
+
+def FACTORS(grid_size: int):
+    return {  # Lower PENALTY values increases factor
+        'DIST_PENALTY': 0.73,
+        'LIFE_PENALTY': 18,
+        'AMMO_PENALTY': 1.5,
+        'MAP_MAX_DISTANCE': (grid_size ** 2 + grid_size ** 2) ** 0.5
+    }
