2f066b804fac366addb465a184570dace31f35b4
Mistayan <helixmastaz@gmail.com>
2023-02-25 15:16:34 +0100

    renamed and reworked bruteforce for optimized output

--- src/utils/algo/tsp/optimized_bruteforce.py
+++ src/utils/algo/tsp/optimized_bruteforce.py
@@ -1,17 +1,13 @@
-import itertools
-from functools import partial
-from multiprocessing import Pool, cpu_count
 from typing import List, Tuple
 
-from src.utils import mapping
+import networkx as nx
+
 from src.utils.algo.ISolver import ISolver
-from src.utils.mapping import path_distance, rec_find_bests
-from src.utils.metrics import measure_perf
+from src.utils.mapping.arrays import get_city_pairs_distances
+from src.utils.mapping.graphs import entities_to_graph
 
 
-# @measure_perf
 class TSPMap(ISolver):
-    # @measure_perf
     def __init__(self, cities: List[Tuple[str, int, int]]):
         """
         Compute all possible distances between entities given for future uses
@@ -19,7 +15,8 @@ class TSPMap(ISolver):
         """
         self.__cities = cities
         self.__size = len(cities)
-        self.__distances = mapping.get_city_pairs_distances(self.__cities)
+        self.__distances = get_city_pairs_distances(cities)
+        self.__graph = entities_to_graph(cities, self.__distances)
         self.__start_index = 0
         print("entities distances processed")
 
@@ -27,7 +24,7 @@ class TSPMap(ISolver):
     def distances(self):
         return self.__distances.copy()
 
-    @measure_perf
+    # @measure_perf
     def solve(self, start_index: int = 0, end: int = 0, back_to_start=False, max_iter: int = float('inf')):
         """
         Compute the fastest path from permutations of the cities indexes
@@ -44,23 +41,18 @@ class TSPMap(ISolver):
         best_path = None
         best_dist = float('inf')
 
-        # for EVERY possible path
-        possibilities = (
-            (self.__cities, self.__distances, _, 0)
-            for _ in itertools.permutations(range(self.__size))
-            if _[0] == start_index
-        )
+        # Find the optimal path using Dijkstra's algorithm
+        start_node = self.__graph.nodes[self.__cities[start_index][0]]
+        start_node["distance"] = 0
+        queue = [start_node]
+        visited = set()
+        # Extract the best path
+        best_path = [i for i in nx.shortest_path(self.__graph, source=start_node, target=end)]
 
-        # calculate optimal distance to travel
-        res = itertools.starmap(path_distance, possibilities).__iter__()
-        print(f"advanced mapping done")
+        # Convert indexes to city coordinates
+        path_coords = [self.__cities[i] for i in best_path]
 
-        # best_path, best_dist = min(res, key=lambda x: x[1])
-        for p, d in res:
-            if not max_iter > 0:
-                break
-            max_iter -= 1
-            if d < best_dist:
-                best_dist = d
-                best_path = p
-        return list(best_path), best_dist
+        # Calculate total distance
+        total_dist = sum(self.distances[i][j] for i, j in zip(best_path[:-1], best_path[1:]))
+
+        return path_coords, total_dist
