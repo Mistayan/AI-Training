6601b1114f80c6d5ebe9f27458e814c5d7bba3da
Mistayan <helixmastaz@gmail.com>
2023-02-25 15:03:25 +0100

    generate file name from given name by incrementing number

---
+++ src/utils/os_utils.py
@@ -0,0 +1,19 @@
+import os
+
+
+def gen_file(filename: str, save_dir="plots"):
+    "facet_plot.png"
+
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    if os.path.exists(os.path.join(save_dir, filename)):
+        fname = ''
+        fext = '.txt'
+        if filename.__contains__('.'):
+            fname, fext = filename.split(".")
+        for i in range(999):
+            fn = f"{fname}-{i}.{fext}"
+            if not os.path.exists(os.path.join(save_dir, fn)):
+                filename = fn
+                break
+    return os.path.join(save_dir, filename)
