418a26a23fc06b8e108e77877963b6d3fd314afa
Stephen Proust <stephen.proust@systemgie.fr>
2024-12-17 15:41:08 +0100

    update Orientation

--- src/utils/orientation.py
+++ src/utils/orientation.py
@@ -2,7 +2,18 @@ import enum
 
 
 class Orientation(enum.Enum):
-    NORTH: int = 3
     EAST: int = 0
-    SOUTH: int = 1
+    NORTH: int = 1
     WEST: int = 2
+    SOUTH: int = 3
+
+    @staticmethod
+    def from_angle(angle):
+        if 315 <= angle or angle < 45:
+            return Orientation.NORTH
+        elif 45 <= angle < 135:
+            return Orientation.EAST
+        elif 135 <= angle < 225:
+            return Orientation.SOUTH
+        else:  # 225 <= angle < 315
+            return Orientation.WEST
