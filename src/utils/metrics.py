419f354a7be1436bfbc6d2093095a1b66b2b10d3
Stephen Proust <stephen.proust@systemgie.fr>
2024-12-19 19:49:56 +0100

    enhanced metric lib

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -9,26 +9,24 @@ def measure_perf(func, print_inout=False, print_doc=False):
 
     logger = logging.getLogger("measure_perf")
 
-
     @wraps(func)
     def wrapper(*args, **kwargs):
-        """ wrapper is like the function it-self. Requires to run the function/method/class in here"""
-        logger.debug("#" * 45)
-        logger.debug(f'Function: {func.__name__}')
+        """ wrapper is like the function it-self.Requires to run the function/method/class in here"""
         not print_doc or logger.debug(f'Method:{func.__doc__}')
         not print_inout or logger.debug(f"input :{args}{kwargs}")
         tracemalloc.start()
         start_time = perf_counter()
-        res = func(*args, **kwargs)
+
+        res = func(*args, **kwargs)  # Execute request action
+
         not print_inout or logger.debug('Output : %s', res)
-        logger.debug("-" * 40)
         finish_time = perf_counter()
         current, peak = tracemalloc.get_traced_memory()
-        logger.debug(f'Memory usage:\t\t {current / 10 ** 6:.6f} MB')
-        logger.debug(f'Peak memory usage:\t {peak / 10 ** 6:.6f} MB ')
-        logger.debug(f'Time elapsed is seconds: {finish_time - start_time:.6f}')
-        logger.debug("#" * 45)
         tracemalloc.stop()
+        logger.debug(f"{func.__name__}\n" +
+                     f"Time elapsed is seconds: {finish_time - start_time:.6f}\n"
+                     f"Memory usage:\t\t {current / 10 ** 6:.6f} MB\n"
+                     f"Peak memory usage:\t {peak / 10 ** 6:.6f} MB")
         return res
 
     return wrapper
